0f1d29fc47c55b1d01eea39fa879378bb1aaf3f0
Jan-Krystof Kujel Zahradnik <jendazah@gmail.com>
2025-11-10 08:25:23 +0100

    chore: prettify the time displaying

--- internal/display/table.go
+++ internal/display/table.go
@@ -8,19 +8,67 @@ import (
 	"github.com/rodaine/table"
 )
 
-func PrintTasks(task []repository.Task) {
-	headerFmt := color.New(color.FgGreen, color.Underline).SprintfFunc()
-	columnFmt := color.New(color.FgYellow).SprintfFunc()
+type Table struct {
+	headerFmt *color.Color
+	columnFmt *color.Color
+	Table     *table.Table
+}
 
-	tbl := table.New("ID", "Name", "StartTime", "Finished", "RecordedTime")
-	tbl.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
-
-	// TODO: Calculate recorded time -> sum of entries
-	for _, task := range task {
-		startTime := time.Unix(task.StartTimestamp, 0)
-		tbl.AddRow(task.ID, task.Name, startTime.Format(time.Kitchen), task.Finished, task.RecordedTime)
+func NewTable(tbl table.Table) *Table {
+	headerFmt := color.New(color.FgGreen, color.Underline)
+	columnFmt := color.New(color.FgYellow)
+	tbl.
+		WithHeaderFormatter(headerFmt.SprintfFunc()).
+		WithFirstColumnFormatter(columnFmt.SprintfFunc())
+	return &Table{
+		Table:     &tbl,
+		headerFmt: headerFmt,
+		columnFmt: columnFmt,
 	}
+}
 
-	tbl.Print()
+func PrintTasks(tasks []repository.Task) {
+	tbl := NewTable(table.New(
+		"ID",
+		"Name",
+		"StartTime",
+		"Finished",
+		"RecordedTime",
+	))
+	table := *tbl.Table
+	for _, task := range tasks {
+		startTime := time.Unix(task.StartTimestamp, 0)
+		d := time.Duration(task.RecordedTime.Int64) * time.Second
+		table.AddRow(
+			task.ID,
+			task.Name,
+			startTime.Format(time.Kitchen),
+			task.Finished,
+			d.String(),
+		)
+	}
+	table.Print()
+}
 
+func PrintEntries(entries []repository.Entry) {
+	tbl := NewTable(table.New(
+		"ID",
+		"taskId",
+		"StartTime",
+		"Finished",
+		"RecordedTime",
+	))
+	table := *tbl.Table
+	for _, entry := range entries {
+		startTime := time.Unix(entry.StartTimestamp, 0)
+		d := time.Duration(entry.RecordedTime.Int64) * time.Second
+		table.AddRow(
+			entry.ID,
+			entry.TaskID,
+			startTime.Format(time.Kitchen),
+			entry.Finished,
+			d.String(),
+		)
+	}
+	table.Print()
 }
